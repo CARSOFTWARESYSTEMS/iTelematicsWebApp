62ff86567866ec3b653f685f0617ca44fc7d55c7
CAR SOFTWARE SYSTEMS <carsoftwaresystems@gmail.com>
2025-12-16 11:29:01 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  async redirects() {
     return [
-      // Only when request host is ev.engineer (or www.ev.engineer)
       {
         source: "/",
         has: [{ type: "host", value: "ev\\.engineer" }],
         destination: "/ev-engineer",
+        permanent: true,
       },
       {
         source: "/",
         has: [{ type: "host", value: "www\\.ev\\.engineer" }],
         destination: "/ev-engineer",
+        permanent: true,
       },
     ];
   },
